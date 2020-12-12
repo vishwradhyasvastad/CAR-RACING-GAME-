8984f42d538d7871d1f061f8748beed8daa39a8b
Vishwradhya S Vastrad <70585702+vishwradhyasvastad@users.noreply.github.com>
2020-12-12 14:51:28 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,11 +13,11 @@ var cars, car1, car2, car3, car4;
 var track,car1img,car2img,car3img,car4img;
 
 function preload(){
-track = loadImage("../images/track.jpg")
-car1img = loadImage("../images/car1.png")
-car2img = loadImage("../images/car2.png")
-car3img = loadImage("../images/car3.png")
-car4img = loadImage("../images/car4.png")
+track = loadImage("images/track.jpg")
+car1img = loadImage("images/car1.png")
+car2img = loadImage("images/car2.png")
+car3img = loadImage("images/car3.png")
+car4img = loadImage("images/car4.png")
 }
   
 
